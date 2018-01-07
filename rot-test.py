435d4161fe52a3c7fc296be40ad50d3e1103b26f
kirillcheck <32092781+kirillcheck@users.noreply.github.com>
2018-01-07 21:27:45 +0300

    Create rot-test.py

---
+++ rot-test.py
@@ -0,0 +1,13 @@
+«»»
+Этот модуль тестирует функцию шифрования алгоритмом rot13
+«»»
+
+from rot_13 import  *
+import pytest
+
+@ pytest.mark.parametrize ( " count, expected " , [
+  ( « привет » , « uryyb » ),
+  ( « uryyb » , « привет » )])
+
+def  test_summ ( подсчет , ожидаемый ):
+	assert (rot13 (count) == ожидается)
